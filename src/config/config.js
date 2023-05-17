4799801946533a569ec38bb5a0040a8011eac4c4
unknown <sidhodhank2018@gmail.com>
2023-05-17 11:42:03 +0530

    [Sidhodhan]Modify:Connected with data base

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.USERNAME,
+    username: process.env.USERNAME_DB,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
